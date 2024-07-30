197f4af8494ac39ba286a4457689fe2731fdad32
alefra88 <efra.rlz@gmail.com>
2024-07-30 13:35:32 -0600

    Agregar archivos de proyecto.

---
+++ APIRestAlumnos/Controllers/AlumnosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIRestAlumnos.Models.Context;
+using APIRestAlumnos.Models.Entities;
+
+namespace APIRestAlumnos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnosController : ControllerBase
+    {
+        private readonly AlumnosContext _context;
+
+        public AlumnosController(AlumnosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Alumnos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Alumnos>>> GetAlumnos()
+        {
+          if (_context.Alumnos == null)
+          {
+              return NotFound();
+          }
+            return await _context.Alumnos.ToListAsync();
+        }
+
+        // GET: api/Alumnos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Alumnos>> GetAlumnos(int id)
+        {
+          if (_context.Alumnos == null)
+          {
+              return NotFound();
+          }
+            var alumnos = await _context.Alumnos.FindAsync(id);
+
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+
+            return alumnos;
+        }
+
+        // PUT: api/Alumnos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAlumnos(int id, Alumnos alumnos)
+        {
+            if (id != alumnos.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(alumnos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlumnosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Alumnos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Alumnos>> PostAlumnos(Alumnos alumnos)
+        {
+          if (_context.Alumnos == null)
+          {
+              return Problem("Entity set 'AlumnosContext.Alumnos'  is null.");
+          }
+            _context.Alumnos.Add(alumnos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAlumnos", new { id = alumnos.Id }, alumnos);
+        }
+
+        // DELETE: api/Alumnos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAlumnos(int id)
+        {
+            if (_context.Alumnos == null)
+            {
+                return NotFound();
+            }
+            var alumnos = await _context.Alumnos.FindAsync(id);
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+
+            _context.Alumnos.Remove(alumnos);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AlumnosExists(int id)
+        {
+            return (_context.Alumnos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ APIRestAlumnos/Models/Context/AlumnosContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using APIRestAlumnos.Models.Entities;
+
+namespace APIRestAlumnos.Models.Context
+{
+    public partial class AlumnosContext : DbContext
+    {
+        public AlumnosContext()
+        {
+        }
+
+        public AlumnosContext(DbContextOptions<AlumnosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Alumnos> Alumnos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+       
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Modern_Spanish_CI_AS");
+
+            modelBuilder.Entity<Alumnos>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(80)
+                    .IsUnicode(false)
+                    .HasColumnName("correo");
+
+                entity.Property(e => e.Curp)
+                    .HasMaxLength(18)
+                    .IsUnicode(false)
+                    .HasColumnName("curp")
+                    .IsFixedLength();
+
+                entity.Property(e => e.FechaNacimiento)
+                    .HasColumnType("date")
+                    .HasColumnName("fechaNacimiento");
+
+                entity.Property(e => e.IdEstadoOrigen).HasColumnName("idEstadoOrigen");
+
+                entity.Property(e => e.IdEstatus).HasColumnName("idEstatus");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(60)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.PrimerApellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("primerApellido");
+
+                entity.Property(e => e.SegundoApellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("segundoApellido");
+
+                entity.Property(e => e.SueldoMensual)
+                    .HasColumnType("decimal(9, 2)")
+                    .HasColumnName("sueldoMensual");
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(10)
+                    .HasColumnName("telefono")
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
